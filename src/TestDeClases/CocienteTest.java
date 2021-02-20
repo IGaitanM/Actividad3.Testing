031932f71e9d7c835f58f1a76ed10fc493d00274
Carlos Ashley <hdpspain@gmail.com>
2021-02-20 13:42:58 +0100

    Añadido testCociente

---
+++ src/TestDeClases/CocienteTest.java
@@ -0,0 +1,14 @@
+package TestDeClases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CocienteTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
