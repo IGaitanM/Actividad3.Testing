3b64fcb7fc7b5b2e7bcf5244a483330cceab0da2
IGaitanM <ivanovef@gmail.com>
2021-02-20 12:56:51 +0100

    Creo la clase de test para resta

---
+++ src/TestDeClases/RestaTest.java
@@ -0,0 +1,16 @@
+// Pruebas en la clase resta. Iván Gaitán Muñoz.
+
+package TestDeClases;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RestaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
