f6b79de491d2b3fef156e8166b096cb2e6e57387
Carlos Ashley <hdpspain@gmail.com>
2021-02-21 13:05:04 +0100

    Añadido tests de la clase Cociente

--- src/testDeClases/CocienteTest.java
+++ src/testDeClases/CocienteTest.java
@@ -1,14 +1,67 @@
+// Test de la clase Resta. Iván Gaitán Muñoz
+
 package testDeClases;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class CocienteTest {
+import principal.Cociente;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+public class CocienteTest {
 
+	// Test para método dividirDosReales.
+			@Test
+			public void dividirDosReales() {
+				// 1. ARRANGED (preparar)
+				Cociente cociente1 = new Cociente();
+				double numReal1 = 6.0;
+				double numReal2 = 1.5;
+				double esperado = 4.0;
+				// 2. ACT (ejecutar)
+				double obtenido = cociente1.dividirDosReales(numReal1, numReal2);
+				// 3. ASSERT (comprobar)
+				assertEquals(esperado, obtenido);
+			}
+			
+			//Test para método dividirDosEnteros.
+			@Test
+			public void dividirDosEnteros() {
+				// 1. ARRANGED (preparar)
+				Cociente cociente1 = new Cociente();
+				int numEnt1 = 300;
+				int numEnt2 = 5;
+				int esperado = 60;
+				// 2. ACT (ejecutar)
+				int obtenido = cociente1.dividirDosEnteros(numEnt1, numEnt2);
+				// 3. ASSERT (comprobar)
+				assertEquals(esperado, obtenido);
+			}
+			
+			// Test para método inverso.
+			@Test
+			public void inverso() {
+				// 1. ARRANGED (preparar)
+				Cociente cociente1 = new Cociente();
+				double num = 100.0;
+				double esperado = 1/num;
+				// 2. ACT (ejecutar)
+				double obtenido = cociente1.inverso(num);
+				// 3. ASSERT (comprobar)
+				assertEquals(esperado, obtenido);
+			}
+			
+			// Test para método raiz.
+			@Test
+			public void raiz() {
+				// 1. ARRANGED (preparar)
+				Cociente cociente1 = new Cociente();
+				double num = 30.0;
+				double esperado = 5.477225575051661;
+				// 2. ACT (ejecutar)
+				double obtenido = cociente1.raiz(num);
+				// 3. ASSERT (comprobar)
+				assertEquals(esperado, obtenido);
+			}
 }
