8b70e119700d182e3401f71c7084a5ded5ccc8a7
Miguel <vilanovamiguel@hotmail.com>
2021-02-21 11:24:56 +0100

    Creo la clase para probar la clase operaciones

---
+++ src/testDeClases/OperacionesTest.java
@@ -0,0 +1,14 @@
+package testDeClases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OperacionesTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
