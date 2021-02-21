2184564155592f79e7f80815b8e56b45778c0e50
Miguel <vilanovamiguel@hotmail.com>
2021-02-21 12:37:44 +0100

    Creo los test para porcentaje y factorial. el test de porcentaje falla porque el metodo de porcentaje es erroneo

--- src/testDeClases/OperacionesTest.java
+++ src/testDeClases/OperacionesTest.java
@@ -36,4 +36,29 @@ public class OperacionesTest {
 		assertEquals(esperado, obtenido);
 	}
 	
+	@Test
+	public void porcentaje() {
+		// 1. ARRANGED (preparar)
+		Operaciones op= new Operaciones();
+		double numero1 = 4;
+		double numero2 = 16;
+		double esperado = 25;
+		// 2. ACT (Ejecutar)
+		double obtenido = op.porcentaje(numero1, numero2);
+		// 3. Assert (comprobar)
+		assertEquals(esperado, obtenido);
+	}
+	
+	@Test
+	public void factorial() {
+		// 1. ARRANGED (preparar)
+		Operaciones op= new Operaciones();
+		int numero = 5;
+		int esperado = 120;
+		// 2. ACT (Ejecutar)
+		int obtenido = op.factorial(numero);
+		// 3. Assert (comprobar)
+		assertEquals(esperado, obtenido);
+	}
+	
 }
